fa93377e8bff18bf587eafdf577447bd6be52e8c
Gourav <gkawatra22@gmail.com>
2024-08-28 00:35:34 +0530

    Queue

---
+++ src/Learning_queue/queue_1.java
@@ -0,0 +1,19 @@
+package Learning_queue;
+import java.util.*;
+
+public class queue_1 {
+	
+	public static void main (String[] args) {
+		Queue<Integer> q
+		= new ArrayDeque<Integer>();
+		q.offer(10);
+		q.offer(20);
+		q.offer(30);
+		System.out.println(q.size());
+		System.out.println(q.isEmpty());
+		System.out.println(q);
+		
+	}
+	
+
+}
