fa5781177403acae93177f67c8614d789a612216
Gourav <gkawatra22@gmail.com>
2024-10-01 14:02:21 +0530

    hashmap

---
+++ src/objectoriented/Hashmap.java
@@ -0,0 +1,17 @@
+package objectoriented;
+
+
+	import java.util.HashMap;
+
+	public class Hashmap {
+	    public static void main(String args[]) {
+	        
+	        // Create a HashMap of Integer keys and String values
+	        HashMap<Integer, String> hashMap = new HashMap<>();
+	        
+	        // Displaying the HashMap (which is empty at this point)
+	        System.out.println("HashMap Elements: " + hashMap);
+	    }
+	}
+
+
