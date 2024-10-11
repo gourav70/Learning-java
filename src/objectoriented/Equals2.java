05b260f88c46bbc6fe94230727bafd0a4f0edcc3
Gourav <gkawatra22@gmail.com>
2024-10-11 23:39:27 +0530

    equals

---
+++ src/objectoriented/Equals2.java
@@ -0,0 +1,28 @@
+package objectoriented;
+
+public class Equals2 {
+
+	public static void main(String[] args) {
+		
+		String s1 = new String("Hello");
+		String s2 = new String("Hello");
+		
+		if(s1== s2) {
+	System.out.println("s1 and s2 are the same reference");
+		}
+		else {
+			System.out.println("s1 and s2 are different references");
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
